63f2fa13419472ac95a2143d231e224841f3dd04
Shotaro Hirose <shotaro199959@icloud.com>
2020-05-11 15:14:54 +0900

    Firestoreから配列(攻撃力)を取得

--- pages/characters.vue
+++ pages/characters.vue
@@ -1,23 +1,35 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">No.</th>
-            <th class="text-left">キャラクター名</th>
-            <th class="text-left">レア</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="character in characters" :key="character.id">
-            <td>{{ character.number }}</td>
-            <td>{{ character.name }}</td>
-            <td>{{ character.rarity }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-flex xs12 sm8 md6>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">No.</th>
+              <th class="text-left">キャラクター名</th>
+              <th class="text-left">攻撃力(Lv.90)</th>
+              <th class="text-left">攻撃力(Lv.100)</th>
+              <th class="text-left">宝具（色）</th>
+              <th class="text-left">レア</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="character in characters" :key="character.id">
+              <td>{{ character.number }}</td>
+              <td>{{ character.name }}</td>
+              <td>
+                {{ character.atk ? character.atk[0] : '' }}
+              </td>
+              <td>
+                {{ character.atk ? character.atk[1] : '' }}
+              </td>
+              <td>{{ character.card }}</td>
+              <td>{{ character.rarity }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -27,6 +39,7 @@ export default {
     return {
       number: '',
       name: '',
+      atk: [],
       rarity: ''
     }
   },
